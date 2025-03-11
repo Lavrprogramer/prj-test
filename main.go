97b4eb7bad32ed1046efb91cd8278c23548c5879
Lavrprogramer <nomi283623@gmail.com>
2025-03-11 08:38:31 +0200

    MathGame base

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"math/rand"
+)
+
+const (
+	totalPoints       = 50
+	pointsPerQuestion = 10
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	fmt.Println("Вітаємо у гір MathGame!!!")
+
+	for i := 5; i > 0; i-- {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+	start := time.Now()
+	play()
+	finish := time.Now()
+	timeSpent := finish.Sub(start)
+	fmt.Printf("Вітаю! Ти впорався за: %v", timeSpent)
+	time.Sleep(10 * time.Second)
+}
+
+func play() {
+	myPoints := 0
+	for myPoints < totalPoints {
+		x, y := rand.Intn(100), rand.Intn(100)
+
+		fmt.Printf("%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+		if err != nil {
+			fmt.Println("Помилка вводу")
+		} else {
+			if x+y == ansInt {
+				myPoints += pointsPerQuestion
+				fmt.Printf("Ви набрали: %v очок!\n", myPoints)
+			} else {
+				fmt.Println("НЕ ПРАВИЛЬНО!")
+			}
+		}
+	}
 }
